c37e14302bcffbdb38f15bed8c426af0afa75564
Lukas Miskovsky <miskovskylukas@gmail.com>
2014-04-05 19:58:03 +0200

    Finish sign in and out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -17,6 +18,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
