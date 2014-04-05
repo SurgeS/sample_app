0f52df1daa5e9e004d0df82a89808fe9791900ab
Lukas Miskovsky <miskovskylukas@gmail.com>
2014-04-05 11:13:39 +0200

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,10 @@ SampletApp::Application.configure do
   config.assets.compile = true
   config.assets.digest = true
 
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  config.force_ssl = true
+  
   # Code is not reloaded between requests.
   #config.cache_classes = true
 
